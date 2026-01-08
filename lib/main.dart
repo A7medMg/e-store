b77cd1e7f84517df26b3f45ca16a48bbc7964678
A7medMg <asilme023@gmail.com>
2026-01-08 10:12:48 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
-import 'home.dart';
+import 'home/home.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
